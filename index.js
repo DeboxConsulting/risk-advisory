55b3f6e021f3028b3e48e8b3409959c913f9de0d
Debox-Mehank <mehankjain1670@gmail.com>
2021-07-23 13:09:00 +0530

    instructions added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-Reveal.initialize();
+Reveal.initialize({ touch: false }).then(() => {
+  const currentSlide = Reveal.getCurrentSlide();
+  const { h, v, f } = Reveal.getIndices((slide = currentSlide));
+  if (h === 0) {
+    const instructions = document.getElementsByClassName("instructions");
+    instructions[0].style.display = "block";
+  }
+});
+
+Reveal.addEventListener("slidechanged", function (evt) {
+  const { up, down, left, right } = Reveal.availableRoutes();
+
+  const currentSlide = Reveal.getCurrentSlide();
+  const { h, v, f } = Reveal.getIndices((slide = currentSlide));
+
+  // Instructions
+  if (h === 0 || (down && !up)) {
+    const instructions = document.getElementsByClassName("instructions");
+    instructions[0].style.display = "none";
+    instructions[0].style.display = "block";
+  } else {
+    const instructions = document.getElementsByClassName("instructions");
+    instructions[0].style.display = "none";
+  }
+});
